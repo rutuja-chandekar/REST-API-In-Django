983a7f39d4fb3bed267b5277d64092aac96baf54
RutujaChandekar <rutuja.chandekar@trigensoft.com>
2023-01-24 18:06:04 +0530

    rendering template

--- demoapi/urls.py
+++ demoapi/urls.py
@@ -2,7 +2,10 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
+
     path('item/', views.get_data, name='item'),
     path('add/', views.addItem, name= 'addItem'),
     path('delete/', views.deleteItem, name= 'deleteItem'),
+    path('update/', views.updateItem, name= 'updateItem'),
 ]
--- demoapi/views.py
+++ demoapi/views.py
@@ -4,6 +4,13 @@ from .models import Item
 from .serialize import ItemSerializer
 from rest_framework.response import Response
 
+
+
+def home(request):
+    itemlist = Item.objects.all()
+    print(itemlist)
+    return render(request, 'demoapi/itemlist.html', context= {'itemlist': itemlist})
+
 @api_view(['GET'])
 def get_data(request):
     items = Item.objects.all()
@@ -21,8 +28,20 @@ def addItem(request):
 
 @api_view(['DELETE'])
 def deleteItem(request):
-    delItem = Item.objects.get(id)
+    print(request.data['id'])
+    delItem = Item.objects.get(id=request.data['id'])
     delItem.delete()
-    return Response({'msg': 'item deleted !!'},)
+    return Response({'msg': 'item deleted !!'})
+
+
+@api_view(['PUT'])
+def updateItem(request):
+    print(request.data['id'])
+    item = Item.objects.get(id=request.data['id'])
+    serializer = ItemSerializer(item, data=request.data, partial=True)
+    if serializer.is_valid():
+        serializer.save()
+        return Response({'msg': 'data updated !!'}, status = 200)
+    return Response({'msg': 'user not updated !!'}, status = 400)
 
     
